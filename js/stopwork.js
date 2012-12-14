ffce62be7979a868c518083ce44663a42df00f16
Ramsey Nasser <ram@nas.sr>
2012-12-14 00:57:05 -0500

    Revamp stopwork.js
    Contain in single object, implement filter API, clean up slide language

--- js/stopwork.js
+++ js/stopwork.js
@@ -1,48 +1,136 @@
-function get_next_slide (slide) {
-  return slide.next(".slide").size() == 0 ? $(".slide:first") : slide.next(".slide");
+var Stopwork = {
+  parse: function(source) {
+    return source.trim().replace(/\n\/\/.*\n/g, '').replace(/\n\s*/g, '\n').split("\n");
+  },
+
+  compile: function(slide_text) {
+    var slides = this.parse(slide_text);
+    return slides.map(function(slide) {
+      return Stopwork.filter(slide);
+    });
+  },
+
+  filters: [],
+
+  filter: function(slide) {
+    for (var i = this.filters.length - 1; i >= 0; i--) {
+      var filtered_slide = this.filters[i](slide);
+      if(filtered_slide)
+        return filtered_slide;
+    };
+
+    return slide;
+  },
+
+  get_next_slide: function (slide) {
+    return slide.next(".slide").size() == 0 ? $(".slide:first") : slide.next(".slide");
+  },
+
+  get_prev_slide: function (slide) {
+    return slide.prev(".slide").size() == 0 ? $(".slide:last") : slide.prev(".slide");
+  },
+
+  refresh_next_prev: function () {
+    $(".slide").removeClass('next').removeClass('prev');
+    this.get_next_slide($(".slide.current")).addClass('next');
+    this.get_prev_slide($(".slide.current")).addClass('prev');
+  },
+
+  next_slide: function () {
+    var current_slide = $(".slide.current");
+    current_slide.removeClass("current");
+    this.get_next_slide(current_slide).addClass("current");
+
+    $("#navigation .current").html($(".slide").index($(".slide.current")) + 1);
+    this.refresh_next_prev();
+  },
+
+  prev_slide: function () {
+    var current_slide = $(".slide.current");
+    current_slide.removeClass("current");
+    this.get_prev_slide(current_slide).addClass("current");
+
+    $("#navigation .current").html($(".slide").index($(".slide.current")) + 1);
+    this.refresh_next_prev();
+  },
+
+  $container: null,
+
+  present: function(content, container) {
+    if(!container) container = "body";
+
+    $container = $(container);
+
+    $container.addClass('stopwork');
+    $container.addClass('loading');
+
+    // clear and append navigation elements
+    $container.html('\
+      <div id="near-navigation"> \
+        <div id="navigation">\
+          <button class="prev iconic iconic-arrow-left" />\
+          <button class="next iconic iconic-arrow-right" />\
+          <div class="current" />\
+          <div class="total" />\
+        </div>\
+      </div>');
+
+    // keyboard scrolling
+    $(window).keydown(function(e) {
+      if(e.which == 39) { // right
+        Stopwork.next_slide();
+
+      } else if(e.which == 37) { // left
+        Stopwork.prev_slide();
+
+      }
+    });
+
+
+    // navigation button scrolling
+    $("#navigation button.prev").click(function() { Stopwork.prev_slide() });
+    $("#navigation button.next").click(function() { Stopwork.next_slide() });
+
+    // populate slides
+    this.compile(content).forEach(function(slide) { $container.append(slide) })
+
+    // make first slide visible
+    $(".slide:first").addClass('current');
+    // mark all other slides as next slides
+    $(".current + .slide").addClass('next');
+    $(".slide:last").addClass('prev');
+
+    $("#navigation .total").html($(".slide").size());
+    $("#navigation .current").html("1");
+  }
 }
 
-function get_prev_slide (slide) {
-  return slide.prev(".slide").size() == 0 ? $(".slide:last") : slide.prev(".slide");
-}
-
-function refresh_next_prev () {
-  $(".slide").removeClass('next').removeClass('prev');
-  get_next_slide($(".slide.current")).addClass('next');
-  get_prev_slide($(".slide.current")).addClass('prev');
-}
-
-function next_slide () {
-  var current_slide = $(".slide.current");
-  current_slide.removeClass("current");
-  get_next_slide(current_slide).addClass("current");
-
-  $("#navigation .current").html($(".slide").index($(".slide.current")) + 1);
-  refresh_next_prev();
-}
-
-function prev_slide () {
-  var current_slide = $(".slide.current");
-  current_slide.removeClass("current");
-  get_prev_slide(current_slide).addClass("current");
-
-  $("#navigation .current").html($(".slide").index($(".slide.current")) + 1);
-  refresh_next_prev();
-}
-
-function add_slide (content, container) {
+/* text */
+Stopwork.filters.push(function(slide) {
   var new_slide = $("<div class='slide'>");
-  var match;
+  new_slide.addClass("text")
+  new_slide.html(marked(slide));
 
-  /* image slide */
-  if(match = content.match(/\.(png|jpg|jpeg|gif)/i)) {
-    new_slide.addClass("image");
-    if(!content.match(/^http/)) content = "assets/" + content;
-    new_slide.css("background-image", "url(" + content + ")");
-    $(container).append(new_slide);
+  return new_slide;
+})
 
-  /* video slide */
-  } else if(match = content.match(/(youtube|vimeo).*?([\w\d]+)$/)) {
+/* web */
+Stopwork.filters.push(function(slide) {
+  if(match = slide.match(/^http/)) {
+    var new_slide = $("<div class='slide'>");
+    new_slide.addClass("web")
+    var iframe = $("<iframe src='" + slide + "'>");
+    new_slide.append(iframe);
+    return new_slide;
+  }
+
+  return false;
+})
+
+/* videos */
+Stopwork.filters.push(function(slide) {
+  if(match = slide.match(/(youtube|vimeo).*?([\w\d]+)$/)) {
+    var new_slide = $("<div class='slide'>");
     var host = match[1], id = match[2];
 
     new_slide.addClass("web");
@@ -58,78 +146,30 @@ function add_slide (content, container) {
     }
 
     new_slide.append(iframe);
-    $(container).append(new_slide);
-
-  /* web slide */
-  } else if(match = content.match(/^http/)) {
-    new_slide.addClass("web")
-    var iframe = $("<iframe src='" + content + "'>");
-    new_slide.append(iframe);
-    $(container).append(new_slide);
-
-  /* text slide */
-  } else {
-    new_slide.addClass("text")
-    new_slide.html(marked(content));
-    $(container).append(new_slide);
-
-    new_slide.css("margin-top", "-" + new_slide.height()/2 + "px");
-
+    return new_slide;
   }
 
-}
+  return false;
+})
 
-function present(content, container) {
-  if(!container) container = "body";
+/* images */
+Stopwork.filters.push(function(slide) {
+  if(match = slide.match(/\.(png|jpg|jpeg|gif)/i)) {
+    var new_slide = $("<div class='slide'>");
+    new_slide.addClass("image");
+    if(!slide.match(/^http/)) slide = "assets/" + slide;
+    new_slide.css("background-image", "url(" + slide + ")");
 
-  $(container).addClass('stopwork');
-  $(container).addClass('loading');
+    return new_slide;
+  }
 
-  // append navigation elements
-  $(container).html('\
-    <div id="near-navigation"> \
-      <div id="navigation">\
-        <button class="prev iconic iconic-arrow-left" />\
-        <button class="next iconic iconic-arrow-right" />\
-        <div class="current" />\
-        <div class="total" />\
-      </div>\
-    </div>');
-
-  // populate slides
-  for (var i = 0; i < content.length; i++) { add_slide(content[i], container); };
-
-  // make first slide visible
-  $(".slide:first").addClass('current');
-  // mark all other slides as next slides
-  $(".current + .slide").addClass('next');
-  $(".slide:last").addClass('prev');
-
-  $("#navigation .total").html(content.length);
-  $("#navigation .current").html("1");
-}
+  return false;
+})
 
 $(function() {
-  // evaluate stopwork script into slideshow
-  if($("script[type$='stopwork']").size() > 0) {
-    present(eval( '[' + $("script[type$='stopwork']").text() + ']' ));
-  }
+  Stopwork.present($(document).text())
+})
 
-  // keyboard scrolling
-  $(window).keydown(function(e) {
-    if(e.which == 39) { // right
-      next_slide();
-
-    } else if(e.which == 37) { // left
-      prev_slide();
-
-    }
-  });
-
-  // navigation button scrolling
-  $("#navigation button.prev").click(function() { prev_slide() });
-  $("#navigation button.next").click(function() { next_slide() });
-});
 
 window.onload = function() {
   $(".stopwork").removeClass('loading');
