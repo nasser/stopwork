b8ef2d67855ac44e76164dfc46d14d7044c32685
Ramsey Nasser <ram@nas.sr>
2015-05-30 23:19:59 -0400

    Add vine support
    Closes #4

--- lib/stopwork/ext.rb
+++ lib/stopwork/ext.rb
@@ -1,5 +1,24 @@
+require 'viddl-rb'
+require 'nokogiri'
+require 'open-uri'
+
 class String
   def to_title
     gsub(/[-_.\s]([a-zA-Z0-9])/) { $1.upcase }.gsub(/([A-Z])/) { " #{$1}"}.gsub(/^([a-z])/) { $1.upcase }
+  end
+end
+
+class Vine < ViddlRb::PluginBase
+  def self.matches_provider?(url)
+    url.include?("vine.co")
+  end
+
+  # return the url for original video file and title
+  def self.get_urls_and_filenames(url, options = {})
+    doc = Nokogiri::HTML(open(url))
+    url = doc.css("head meta[property='twitter:player:stream']").first.attr("content")
+    title = doc.css("head meta[property='twitter:title']").first.attr("content")
+    
+    [{:url => url, :name => title}]
   end
 end
