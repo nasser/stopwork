c5b7d56060cf4ced8835d3dfbf0945da99662a1c
Ramsey Nasser <ram@nas.sr>
2015-05-30 21:07:13 -0400

    Change video to support HTML 5 video
    Breaks youtube/vimeo special casing

--- lib/stopwork/types/video.rb
+++ lib/stopwork/types/video.rb
@@ -7,39 +7,12 @@ module Stopwork
     class Video < Slide
       # match any youtube or vimeo url
       def self.match? slide
-        !! (slide =~ /(youtube|vimeo).*?([\w\d]+)$/)
-      end
-
-      # true if this is a youtube video
-      def youtube?
-        @host =~ /youtube/
-      end
-
-      # true if this is a vimeo
-      def vimeo?
-        @host =~ /vimeo/
+        !! (slide =~ /\.(mp4|ogg|mov|webm)/i or slide =~/^data:video/i)
       end
 
       # populate the div with the appropriate youtube/vimeo iframe
       def template
-        <<-TEMPLATE
-<div class="slide web video">
-  {{#youtube?}}
-    <iframe src='http://www.youtube-nocookie.com/embed/{{id}}?rel=0&showinfo=0&autohide=1&controls=0'></iframe>
-  {{/youtube?}}
-  {{#vimeo?}}
-    <iframe src='http://player.vimeo.com/video/{{id}}?badge=0&title=0&portrait=0&byline=0'></iframe>
-  {{/vimeo?}}
-</div>
-TEMPLATE
-      end
-
-      attr_reader :id
-      def initialize slide
-        super
-        match = slide.match(/(youtube|vimeo).*?([\w\d]+)$/)
-        @host = match[1]
-        @id = match[2]
+        '<div class="slide video"><video preload=true src="{{slide}}"" controls></video></div>'
       end
     end
   end
