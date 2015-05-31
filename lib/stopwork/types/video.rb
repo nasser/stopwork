a91d6371dc703d6ebd570d6604ab089d572efeca
Ramsey Nasser <ram@nas.sr>
2015-05-30 21:13:03 -0400

    Fix typo in video template

--- lib/stopwork/types/video.rb
+++ lib/stopwork/types/video.rb
@@ -12,7 +12,7 @@ module Stopwork
 
       # populate the div with the appropriate video tag
       def template
-        '<div class="slide video"><video preload=true src="{{slide}}"" controls></video></div>'
+        '<div class="slide video"><video preload=true src="{{slide}}" controls></video></div>'
       end
     end
   end
